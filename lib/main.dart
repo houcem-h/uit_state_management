82754780c020edbc57328dd863c007eba8a9f71d
houcem <hedhoucem@gmail.com>
2022-04-11 10:48:33 +0100

    seperate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './my_home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,63 +18,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Random color App', color: Colors.indigo),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title, required this.color}) : super(key: key);
-
-  final String title;
-  Color color;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: widget.color,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'The color is',
-              style: TextStyle(
-                fontSize: 25
-                ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.13,
-              color: widget.color,
-              child: const Center(
-                child: Text(
-                  'R(), G(), B()',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-            )],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Change color',
-        child: const Icon(Icons.palette),
-        backgroundColor: widget.color,
-      ),
     );
   }
 }
---
+++ lib/my_home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title, required this.color}) : super(key: key);
+
+  final String title;
+  Color color;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: widget.color,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'The color is',
+              style: TextStyle(
+                  fontSize: 25
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.13,
+              color: widget.color,
+              child: const Center(
+                child: Text(
+                  'R(), G(), B()',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            )],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Change color',
+        child: const Icon(Icons.palette),
+        backgroundColor: widget.color,
+      ),
+    );
+  }
+}
